e197d12f71bf76c77b7b3123617c6b946701fca3
CARLOS RAMÍREZ BELTRÁN <44666182+carlos1521@users.noreply.github.com>
2019-11-02 12:39:07 -0300

    version 1.9-cb
    delete de pastas e arquivos sem uso.

--- lib/model/homelist.dart
+++ lib/model/homelist.dart
@@ -1,6 +1,4 @@
 import 'package:best_flutter_ui_templates/designCourse/homeDesignCourse.dart';
-import 'package:best_flutter_ui_templates/fitnessApp/fitnessAppHomeScreen.dart';
-import 'package:best_flutter_ui_templates/hotelBooking/hotelHomeScreen.dart';
 import 'package:flutter/widgets.dart';
 
 class HomeList {
@@ -15,15 +13,15 @@ class HomeList {
   static List<HomeList> homeList = [
     HomeList(
       imagePath: "assets/hotel/hotel_booking.png",
-      navigateScreen: HotelHomeScreen(),
+      //navigateScreen: HotelHomeScreen(),
     ),
     HomeList(
       imagePath: "assets/fitness_app/fitness_app.png",
-      navigateScreen: FitnessAppHomeScreen(),
+      //navigateScreen: FitnessAppHomeScreen(),
     ),
     HomeList(
       imagePath: "assets/design_course/design_course.png",
-      navigateScreen: DesignCourseHomeScreen(),
+      //navigateScreen: DesignCourseHomeScreen(),
     ),
   ];
 }
