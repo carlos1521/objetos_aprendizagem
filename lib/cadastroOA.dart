1d39ba418630898862e9fc37daa53085a6ea7c03
CARLOS RAMÍREZ BELTRÁN <44666182+carlos1521@users.noreply.github.com>
2019-11-07 20:14:46 -0300

    versao 2.0-cb
    melhoria das telas de cadastro de OA

--- lib/cadastroOA.dart
+++ lib/cadastroOA.dart
@@ -4,11 +4,117 @@ class CadastroOA extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-        appBar: new AppBar(title: new Text("CADASTRO OA"),
+        appBar: new AppBar(title: new Text("Cadastro de Novo OA"),
         ),
-        body: new Center(
-          child: new Text("Em Manutenção"),
-        )
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+          child: RegisterForm(),
+        ),
+      ),
     );
+  }
+}
+
+class RegisterForm extends StatefulWidget {
+  const RegisterForm({Key key}) : super(key: key);
+
+  @override
+  _RegisterFormState createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _agreedToTOS = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Tipo',
+            ),
+            validator: (String value) {
+              if (value.trim().isEmpty) {
+                return 'Tipo é obrigatorio';
+              }
+            },
+          ),
+          const SizedBox(height: 16.0),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Área',
+            ),
+            validator: (String value) {
+              if (value.trim().isEmpty) {
+                return 'Área é obrigatorio';
+              }
+            },
+          ),
+          const SizedBox(height: 16.0),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Título',
+            ),
+            validator: (String value) {
+              if (value.trim().isEmpty) {
+                return 'Título é obrigatoria';
+              }
+            },
+          ),
+          const SizedBox(height: 16.0),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Descrição',
+            ),
+            validator: (String value) {
+              if (value.trim().isEmpty) {
+                return 'Descrição é obrigatorio';
+              }
+            },
+          ),
+          const SizedBox(height: 16.0),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'URL',
+            ),
+            validator: (String value) {
+              if (value.trim().isEmpty) {
+                return 'URL é obrigatorio';
+              }
+            },
+          ),          
+          Row(
+            children: <Widget>[
+              const Spacer(),
+              OutlineButton(
+                highlightedBorderColor: Colors.black,
+                onPressed: _submittable() ? _submit : null,
+                child: const Text('Cadastrar'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool _submittable() {
+    return _agreedToTOS;
+  }
+
+  void _submit() {
+    _formKey.currentState.validate();
+    print('Form submitted');
+  }
+
+  void _setAgreedToTOS(bool newValue) {
+    setState(() {
+      _agreedToTOS = newValue;
+    });
   }
 }
--- lib/pages/meusoa_page.dart
+++ lib/pages/meusoa_page.dart
@@ -1,14 +1,132 @@
+import 'package:best_flutter_ui_templates/cadastroOA.dart';
 import 'package:flutter/material.dart';
 
 class MeusOA extends StatelessWidget{
   @override
+
+
   Widget build(BuildContext context){
     return Scaffold(
         appBar: new AppBar(title: new Text("Meus OAs"),
         ),
-        body: new Center(
-          child: new Text("Em Manutenção"),
-        )
+         body: new Container(
+          child: new ListView(
+          children: [
+            Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const ListTile(
+                  leading: Icon(Icons.video_library),
+                  title: Text('Historia do Brasil'),
+                  subtitle: Text('Video explicativo da Historia do Brasil.'),
+                ),
+                ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                  child: ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: const Text('Editar'),
+                        onPressed: () { /* ... */ },
+                      ),
+                      FlatButton(
+                        child: const Text('Eliminar'),
+                        onPressed: () { /* ... */ },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const ListTile(
+                  leading: Icon(Icons.video_library),
+                  title: Text('Descobrimento ou Conquista do Brasil'),
+                  subtitle: Text('Uma professora de história para falar das polêmicas à respeito do Descobrimento do Brasil.'),
+                ),
+                ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                  child: ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: const Text('Editar'),
+                        onPressed: () { /* ... */ },
+                      ),
+                      FlatButton(
+                        child: const Text('Eliminar'),
+                        onPressed: () { /* ... */ },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const ListTile(
+                  leading: Icon(Icons.school),
+                  title: Text('História geral do Brasil (Português)'),
+                  subtitle: Text('A História Geral do Brasil é um balanço moderno, atual e renovador do conjunto da História do Brasil, com a participação de historiadores consagrados por suas pesquisas e livros .'),
+                ),
+                ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                  child: ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: const Text('Editar'),
+                        onPressed: () { /* ... */ },
+                      ),
+                      FlatButton(
+                        child: const Text('Eliminar'),
+                        onPressed: () { /* ... */ },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const ListTile(
+                  leading: Icon(Icons.games),
+                  title: Text('Jogos de Geografia'),
+                  subtitle: Text('Esta seção é dedicada ao aprendizado de um jeito descontraído, estimulando a capacidade de interpretação do conteúdo a partir de jogos.'),
+                ),
+                ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                  child: ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: const Text('Editar'),
+                        onPressed: () { /* ... */ },
+                      ),
+                      FlatButton(
+                        child: const Text('Eliminar'),
+                        onPressed: () { /* ... */ },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+      onPressed: () {
+        new CadastroOA();
+        //print('Clicked');
+      },
+      child: Icon(Icons.add),
+      backgroundColor: Colors.blue,
+    ),
+        
     );
   }
 }
